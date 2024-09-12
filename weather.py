970fd7f08fea5f18a2ff8a81f63bb3edff26778e
kekedezha <dezhac6@gmail.com>
2024-09-11 22:04:15 -0400

    updated get weather function

--- weather.py
+++ weather.py
@@ -7,3 +7,12 @@ load_dotenv()
 
 def get_current_weather(city="Charlotte"):
     request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+
+    weather_data = requests.get(request_url).json()
+
+    return weather_data
+
+if __name__ == "__main__":
+    print("\n***Get Current Weather Conditions***\n")
+
+    city 
