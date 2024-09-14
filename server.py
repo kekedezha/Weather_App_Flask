7336680ffa6c0ccb16721d025440a87eb575a3df
kekedezha <dezhac6@gmail.com>
2024-09-13 22:54:28 -0400

    updated server.py with simple code to start setting up app

--- server.py
+++ server.py
@@ -1,1 +1,14 @@
 # File to be used as a Flask instance to run our server. 
+from flask import Flask, render_template, request
+from weather import get_current_weather
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
